57f5e50fa1debd087c180484d7098092405df9a0
Florian Favre <32709919+FlorianFavre@users.noreply.github.com>
2022-06-14 11:44:27 +0200

    Small modify

--- Voting.sol
+++ Voting.sol
@@ -45,20 +45,6 @@ contract Voting is Ownable {
     constructor(){
 
         votantArray.push(owner());//le owner a le droit de voter et de faire des propositions
-        votantArray.push(0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2);
-        votantArray.push(0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db);
-        votantArray.push(0x78731D3Ca6b7E34aC0F824c42a7cC18A495cabaB);
-        votantArray.push(0x617F2E2fD72FD9D5503197092aC168c91465E7f2);
-        votantArray.push(0x17F6AD8Ef982297579C203069C1DbfFE4348c372);
-        votantArray.push(0x5c6B0f7Bf3E7ce046039Bd8FABdfD3f9F5021678);
-        votantArray.push(0x03C6FcED478cBbC9a4FAB34eF9f40767739D1Ff7);
-        votantArray.push(0x1aE0EA34a72D944a8C7603FfB3eC30a6669E454C);
-        votantArray.push(0x0A098Eda01Ce92ff4A4CCb7A4fFFb5A43EBC70DC);
-        votantArray.push(0xCA35b7d915458EF540aDe6068dFe2F44E8fa733c);
-        votantArray.push(0x14723A09ACff6D2A60DcdF7aA4AFf308FDDC160C);
-        votantArray.push(0x4B0897b0513fdC7C541B6d9D7E929C4e5364D2dB);
-        votantArray.push(0x583031D1113aD414F02576BD6afaBfb302140225);
-        votantArray.push(0xdD870fA1b7C4700F2BD7f44238821C26f7392148);
         winningProposalId = 0;
         status = WorkflowStatus.RegisteringVoters;
         //je rajoute le vote nul en indice 0 car pour chaque votant votedProposalId étant initialisé à 0 on ne pouvait pas savoir si un votant avait effectivement voter pour la proposition zero  
@@ -74,10 +60,11 @@ contract Voting is Ownable {
     }
 
     //ajoute un votant
-    function addVoter (address addMore) internal onlyOwner {
+    function addVoter (address _addMore) public onlyOwner {
         
-        votantMap[addMore] = Voter(true, false, 0);
-        emit VoterRegistered(addMore);
+        votantMap[_addMore] = Voter(true, false, 0);
+        votantArray.push(_addMore);
+        emit VoterRegistered(_addMore);
     }
 
     //l'admin enregistre les votants ici puis j'attends les propositions
