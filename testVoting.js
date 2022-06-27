93cec8bac286d6588e4a2525c7a650671d783380
Florian Favre <32709919+FlorianFavre@users.noreply.github.com>
2022-06-27 18:34:57 +0200

    Update testVoting.js

--- testVoting.js
+++ testVoting.js
@@ -33,7 +33,7 @@ contract('Voting2', accounts => {
         it("addProposal empty was test", async () => {
 
             await votingInstance.addVoter(accounts[1]);
-            await votingInstance.startProposalsRegistering();
+            await votingInstance.startProposalsRegistering({from:owner});
             await expectRevert(votingInstance.addProposal("", {from:accounts[1]}), 'vous ne pouvez pas mettre une valeur nulle');
 
         });
@@ -41,11 +41,46 @@ contract('Voting2', accounts => {
         it("good addProposal was accept", async () => {
 
             await votingInstance.addVoter(accounts[2]);
-            await votingInstance.startProposalsRegistering();
+            await votingInstance.startProposalsRegistering({from:owner});
             const resAddV = await votingInstance.addProposal("Lucky", {from:accounts[2]});
             expectEvent(resAddV, 'ProposalRegistered', { 
                 proposalId: new BN(0),
             });
+
+        });
+
+        it("test vote have a winner", async () => {
+
+            await votingInstance.addVoter(accounts[1]);
+            await votingInstance.addVoter(accounts[2]);
+            await votingInstance.addVoter(accounts[3]);
+            await votingInstance.addVoter(accounts[4]);
+            await votingInstance.addVoter(accounts[5]);
+            await votingInstance.addVoter(accounts[6]);
+            await votingInstance.addVoter(accounts[7]);
+            
+            await votingInstance.startProposalsRegistering({from:owner});
+            await votingInstance.addProposal("Premiere proposition", {from:accounts[1]});
+            await votingInstance.addProposal("Seconde proposition", {from:accounts[2]});
+            await votingInstance.addProposal("Troisieme proposition", {from:accounts[3]});
+            await votingInstance.addProposal("Quatrième proposition", {from:accounts[4]});
+
+            
+            await votingInstance.endProposalsRegistering({from:owner});
+            await votingInstance.startVotingSession({from:owner});
+
+            await votingInstance.setVote(0, {from:accounts[1]});
+            await votingInstance.setVote(0, {from:accounts[2]});
+            await votingInstance.setVote(1, {from:accounts[3]});
+            await votingInstance.setVote(3, {from:accounts[4]});
+            await votingInstance.setVote(2, {from:accounts[5]});
+            await votingInstance.setVote(3, {from:accounts[6]});
+            await votingInstance.setVote(3, {from:accounts[7]});
+
+            await votingInstance.endVotingSession({from:owner});
+            await votingInstance.tallyVotes({from:owner});
+
+            expect( await votingInstance.winningProposalID.call()).to.be.bignumber.equal(new BN(3));
 
         });
 
